54e9326567e1d50b13d1a8c965f5b5fdf88ba90d
hakimdaniel <mika259123321@gmail.com>
2024-09-17 01:04:14 +0800

    Delete src directory

--- src/script.js
+++
@@ -1,8 +0,0 @@
-function copy(id) {
-            const select = document.getElementById(id).textContent
-            navigator.clipboard.writeText(select)
-            .then(function() {
-                alert("Success copy id : "+select)})
-            .catch(function() {
-                console.error("Error copy id : "+id)})
-}
